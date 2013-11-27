d9b5241c41746e96e3d3655942bb4333912ce20d
Jonathan Underwood <jonathan.underwood@gmail.com>
2013-11-27 21:11:15 +0000

    Run commands as user admin in automount.pp

--- manifests/automount.pp
+++ manifests/automount.pp
@@ -1,9 +1,7 @@
 # Note that these are very rough and ready - these should be
 # implemented properly as custom types. The challenge of that is
 # parsing the output of the ipa commands in Ruby. Or, better, using
-# the JSON interface. A nice example of simple processing of command
-# line program output can be found in:
-# http://serverfault.com/questions/127460/how-do-i-install-a-yum-package-group-with-puppet
+# the JSON interface.
 
 define ipa::automount::location (
   $location_name,
@@ -14,13 +12,13 @@ define ipa::automount::location (
 
   if $ensure == 'present' {
     exec {"automountlocation-$location_name":
-      command => "ipa automountlocation-add $location_name",
-      unless  => "ipa automountlocation-show $location_name > /dev/null 2>&1",
+      command => "runuser -l admin -c 'ipa automountlocation-add $location_name'",
+      unless  => "runuser -l admin -c 'ipa automountlocation-show $location_name > /dev/null 2>&1'",
     }
   } else {
     exec {"automountlocation-remove-$location_name":
-      command => "ipa automountlocation-del $location_name",
-      onlyif  => "ipa automountlocation-show $location_name > /dev/null 2>&1",
+      command => "runuser -l admin -c 'ipa automountlocation-del $location_name'",
+      onlyif  => "runuser -l admin -c 'ipa automountlocation-show $location_name > /dev/null 2>&1'",
     }
   }
 }
@@ -39,8 +37,8 @@ define ipa::automount::map (
   if $ensure == 'present' {
     if $indirect == false {
       exec {"automountmap-$map_name":
-        command => "ipa automountmap-add $location $map_name",
-        unless  => "ipa automountmap-show $location $map_name > /dev/null 2>&1",
+        command => "runuser -l admin -c 'ipa automountmap-add $location $map_name'",
+        unless  => "runuser -l admin -c 'ipa automountmap-show $location $map_name > /dev/null 2>&1'",
       }
     } else {
       if $parent_map {
@@ -50,14 +48,14 @@ define ipa::automount::map (
       }
         
       exec {"automountmap-$map_name":
-        command => "ipa automountmap-add-indirect $location $map_name --mount=$mount_location $parentmap",
-        unless  => "ipa automountmap-show $location $map_name > /dev/null 2>&1",
+        command => "runuser -l admin -c 'ipa automountmap-add-indirect $location $map_name --mount=$mount_location $parentmap'",
+        unless  => "runuser -l admin -c 'ipa automountmap-show $location $map_name > /dev/null 2>&1'",
       }
     }
   } else {
     exec {"automountmap-remove-$map_name":
-      command => "ipa automountmap-del $location $map_name",
-      onlyif  => "ipa automountmap-show $location $map_name > /dev/null 2>&1",
+      command => "runuser -l admin -c 'ipa automountmap-del $location $map_name'",
+      onlyif  => "runuser -l admin -c 'ipa automountmap-show $location $map_name > /dev/null 2>&1'",
     }
   }
 }
@@ -74,13 +72,13 @@ define ipa::automount::key (
 
   if $ensure == 'present' {
     exec {"automountkey-$key_name":
-      command => "ipa automountkey-add $location $map --key=$key_name --info=$mount_info",
-      unless  => "ipa automountkey-show $location $map --key=$key_name >/dev/null 2>&1",
+      command => "runuser -l admin -c 'ipa automountkey-add $location $map --key=$key_name --info=$mount_info'",
+      unless  => "runuser -l admin -c 'ipa automountkey-show $location $map --key=$key_name >/dev/null 2>&1'",
     }
   } else {
     exec {"automountkey-remove-$key_name":
-      command => "ipa automountkey-del $location $map --key=$key_name",
-      onlyif  => "ipa automountkey-show $location $map --key=$key_name >/dev/null 2>&1",
+      command => "runuser -l admin -c 'ipa automountkey-del $location $map --key=$key_name'",
+      onlyif  => "runuser -l admin -c 'ipa automountkey-show $location $map --key=$key_name >/dev/null 2>&1'",
     }
   }
 }
